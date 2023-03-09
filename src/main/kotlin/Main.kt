716d1e9e1b1269c2c6b9c62cf626fa7becce48bb
wainainalinahlinah <wainainalinahw@gmail.com>
2023-03-09 08:52:28 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,48 @@
+fun main() {
+   length(30,num2:40)
+    modules(1,80,50)
+    addition(30,40)
+    subration(60,40)
+    modules(25,5)
+    mutiplication(50,60)
+
+
+}
+fun length(num1: String :num2:String){
+    var length=(num1+num2)
+    return length
+}
+fun modules(num1:Int,num2:Int,num3:Int,){
+    var modules=(num1%num2%num3%)
+    return modules
+}
+var text = "Akirachix"{
+println(text[1])//first element
+println(text[3])//second element
+println(text[4])//fourth element
+{
+    fun addition(num1:Int,num2:Int,num3:Int,num4:){
+        var addition=(num1+num2+num3+num4)
+        return addition
+
+    }    }
+    fun addition(num1:String,num2:String){
+        var addition=(num1+num2)
+        return (addition)
+
+    }    }
+    fun subration(num1:String,num2:String){
+        var subration=(num1-num2)
+        return subration
+    }
+fun modules(num1:String,num2:String){
+    var modules=(num1%num2)
+    return modules
+}
+fun mutiplication(num1:Int,num2:Int)
+var mutiplication=(num1*num2)
+return mutiplication
+
+
+
+
